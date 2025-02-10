7046599a53cbe49c0141c9c2d35cfa06983f13ae
euics <pjh575157@gmail.com>
2025-02-10 12:16:03 +0900

    feat: MemberServiceMapper 추가 (#3)
    - `MemberServiceMapper` 인터페이스를 추가하여 DTO → Entity 변환 기능 구현
    - `toMemberEntity` 메서드를 추가하여 `SaveMemberRequestDto`를 `MemberEntity`로 변환
      - `memberSeq`는 자동 생성되므로 매핑 제외
      - `id`, `userName`은 `requestDto`에서 매핑
      - `password`는 인코딩된 비밀번호(`encodedPassword`)로 설정

---
+++ service/src/main/java/assignment/application/service/member/mapper/MemberServiceMapper.java
@@ -0,0 +1,19 @@
+package assignment.application.service.member.mapper;
+
+import static assignment.application.service.member.dto.request.MemberRequestDto.*;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import assignment.application.infrastructure.member.entity.MemberEntity;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MemberServiceMapper {
+	@Mapping(target = "memberSeq", ignore = true)
+	@Mapping(target = "id", source = "requestDto.id")
+	@Mapping(target = "password", source = "encodedPassword")
+	@Mapping(target = "userName", source = "requestDto.userName")
+	MemberEntity toMemberEntity(SaveMemberRequestDto requestDto, String encodedPassword);
+}
