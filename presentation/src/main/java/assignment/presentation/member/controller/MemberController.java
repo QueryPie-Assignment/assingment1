9171007459ffbf0de26e9b977a650a7c0589bf47
euics <pjh575157@gmail.com>
2025-02-10 13:02:40 +0900

    feat: 로그인 API 기능 구현 (#3)
    - 성공 시 HTTP 200 응답 (`BaseResponse<LoginMemberResponse>` 반환)
    - `BadRequestException` 발생 시 예외 처리 추가:
      - `INF001`: 404 존재하지 않는 아이디 (ID_NOT_FOUND_EXCEPTION)
      - `IPMB002`: 400 아이디 비밀번호 불일치 (ID_PASSWORD_MISMATCH_BAD_REQUEST_EXCEPTION)
      - `B001`: 400 요청 데이터 형식 오류 (DTO_BAD_REQUEST_EXCEPTION)

--- presentation/src/main/java/assignment/presentation/member/controller/MemberController.java
+++ presentation/src/main/java/assignment/presentation/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package assignment.presentation.member.controller;
 
+import static assignment.application.service.member.dto.response.MemberResponseDto.*;
 import static assignment.presentation.member.vo.request.MemberRequest.*;
+import static assignment.presentation.member.vo.response.MemberResponse.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import assignment.application.exception.GlobalExceptionHandler;
 import assignment.application.service.member.service.MemberService;
 import assignment.presentation.base.BaseResponse;
+import assignment.presentation.member.mapper.MemberControllerMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -27,10 +30,12 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 	private final MemberService memberService;
 
-	@PostMapping()
+	private final MemberControllerMapper memberControllerMapper;
+
+	@PostMapping("/save")
 	@Operation(summary = "회원가입 API", description = "회원 정보를 입력하여 가입을 진행합니다.")
 	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "회원가입 성공",
+		@ApiResponse(responseCode = "201", description = "201 회원가입 성공",
 			content = @Content(schema = @Schema(implementation = BaseResponse.class))),
 		@ApiResponse(responseCode = "B001", description = "400 요청 데이터 형식 오류 (BadRequestException 발생)",
 			content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
@@ -45,6 +50,28 @@ public class MemberController {
 
 		memberService.saveMember(request.toSaveMemberRequestDto());
 
-		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+		return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+
+	@PostMapping("/login")
+	@Operation(summary = "로그인 API", description = "회원 정보를 입력하여 로그인을 진행합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "회원가입 성공",
+			content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+		@ApiResponse(responseCode = "B001", description = "400 요청 데이터 형식 오류 (BadRequestException 발생)",
+			content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(responseCode = "INF001", description = "404 존재하지 않는 아이디입니다. (BadRequestException 발생)",
+			content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(responseCode = "IPMB002", description = "400 아이디 비밀번호가 틀렸습니다. (BadRequestException 발생)",
+			content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+	})
+	public ResponseEntity<BaseResponse<LoginMemberResponse>> loginMember(@RequestBody LoginMemberRequest request) {
+		// Request 유효성 검사
+		request.validateLoginMemberRequest();
+
+		LoginMemberResponseDto responseDto = memberService.loginMember(request.toLoginMemberRequestDto());
+		LoginMemberResponse response = memberControllerMapper.toLoginMemberResponse(responseDto);
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
 	}
 }
