ba224c3705cc1d66283b7e469dec6293f19889de
euics <pjh575157@gmail.com>
2025-02-10 12:17:12 +0900

    feat: Spring Security 설정 추가 (#3)
    - `SecurityFilterChain`을 정의하여 HTTP 보안 설정 적용
      - `csrf`, `formLogin`, `httpBasic` 비활성화
      - 모든 요청(`/**`)을 허용하되, 기본적으로 인증이 필요한 요청을 처리하도록 설정
      - 세션 관리 정책을 `STATELESS`로 설정하여 JWT 인증 방식 설정
    - `BCryptPasswordEncoder`를 빈으로 등록하여 비밀번호 암호화 설정

---
+++ core/src/main/java/assignment/application/core/configuration/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package assignment.application.core.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/**").permitAll()
+				.anyRequest().authenticated())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+		return http.build();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
