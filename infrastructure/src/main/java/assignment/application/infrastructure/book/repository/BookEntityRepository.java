09a0b4b81981a63f059aa2bb68afa44ea182bb55
euics <pjh575157@gmail.com>
2025-02-10 16:17:05 +0900

    feat: Book Repo 구현 (#7)
    - `save`: 도서 저장 메서드 구현
    - `findBySeq`: 도서 ID로 조회, 없을 경우 예외 처리
    - `findAllBooks`: 페이징 처리된 도서 목록 조회
    - `findBooksByTag`: 특정 태그 기반 도서 조회 기능 추가
    - `deleteBook`: 도서 삭제 기능 구현

--- infrastructure/src/main/java/assignment/application/infrastructure/book/repository/BookEntityRepository.java
+++ infrastructure/src/main/java/assignment/application/infrastructure/book/repository/BookEntityRepository.java
@@ -1,8 +1,23 @@
 package assignment.application.infrastructure.book.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+
+import assignment.application.infrastructure.book.entity.BookEntity;
+import assignment.application.infrastructure.book.entity.BookTag;
 
 @Component
 public interface BookEntityRepository {
+	BookEntity save(BookEntity bookEntity);
 
+	BookEntity findBySeq(Long bookSeq);
+
+	Page<BookEntity> findAllBooks(Pageable pageable);
+
+	List<BookEntity> findBooksByTag(BookTag bookTag);
+
+	void deleteBook(BookEntity bookEntity);
 }
