b47c62f2954b3a3cc6d51fd0600aa2ee2fe814e1
euics <pjh575157@gmail.com>
2025-02-10 09:52:24 +0900

    feat: MemberEntity 엔티티 추가 (#1)
    - MemberEntity 엔티티 클래스 생성
      - memberSeq, userName, password 필드 추가
      - @DynamicInsert 및 @DynamicUpdate 적용

---
+++ infrastructure/src/main/java/assignment/application/infrastructure/member/entity/MemberEntity.java
@@ -0,0 +1,36 @@
+package assignment.application.infrastructure.member.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "MEMBER")
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberEntity {
+	@Id
+	@Column(name = "member_seq")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberSeq;
+
+	@Column(nullable = false, name = "user_name")
+	private String userName;
+
+	@Column(nullable = false, name = "password")
+	private String password;
+}
