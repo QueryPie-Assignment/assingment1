77d809624d0a306777d8c827a3f38a33b17b96e3
euics <pjh575157@gmail.com>
2025-02-10 09:54:53 +0900

    feat: RepositoryImpl 클래스 추가 (#1)
    - MemberEntityRepository 구현 클래스 생성

---
+++ infrastructure/src/main/java/assignment/application/infrastructure/loan/repository/LoanEntityRepositoryImpl.java
@@ -0,0 +1,7 @@
+package assignment.application.infrastructure.loan.repository;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoanEntityRepositoryImpl implements LoanEntityRepository {
+}
---
+++ infrastructure/src/main/java/assignment/application/infrastructure/member/repository/MemberEntityRepositoryImpl.java
@@ -0,0 +1,7 @@
+package assignment.application.infrastructure.member.repository;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberEntityRepositoryImpl implements MemberEntityRepository {
+}
