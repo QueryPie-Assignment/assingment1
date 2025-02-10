244575da4e2916a3be62ff3a80028b9cf53f8e27
euics <pjh575157@gmail.com>
2025-02-10 17:33:15 +0900

    feat: 대출 데이터 저장 및 조회 기능 추가 (#9)
    - `LoanEntityRepositoryImpl` 구현
      - `save`: 대출 정보 저장 기능 추가
      - `findByMemberEntityAndBookEntity`: 특정 회원이 대출한 도서 조회 기능 추가
        - `QLoanEntity`를 사용하여 `memberEntity`와 `bookEntity` 기반으로 조회
        - 데이터가 존재하지 않을 경우 `NotFoundRequestException` 예외 발생
      - `delete`: 대출 정보 삭제 기능 추가

--- infrastructure/src/main/java/assignment/application/infrastructure/loan/repository/LoanEntityRepositoryImpl.java
+++ infrastructure/src/main/java/assignment/application/infrastructure/loan/repository/LoanEntityRepositoryImpl.java
@@ -1,7 +1,46 @@
 package assignment.application.infrastructure.loan.repository;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Component;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import assignment.application.exception.message.ErrorResult;
+import assignment.application.exception.status.NotFoundRequestException;
+import assignment.application.infrastructure.book.entity.BookEntity;
+import assignment.application.infrastructure.loan.entity.LoanEntity;
+import assignment.application.infrastructure.loan.entity.QLoanEntity;
+import assignment.application.infrastructure.member.entity.MemberEntity;
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class LoanEntityRepositoryImpl implements LoanEntityRepository {
+	private final LoanEntityJpaRepository loanEntityJpaRepository;
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public LoanEntity save(LoanEntity loanEntity) {
+		return loanEntityJpaRepository.save(loanEntity);
+	}
+
+	@Override
+	public LoanEntity findByMemberEntityAndBookEntity(MemberEntity memberEntity, BookEntity bookEntity) {
+		QLoanEntity qLoanEntity = QLoanEntity.loanEntity;
+
+		return Optional.ofNullable(
+			queryFactory
+				.selectFrom(qLoanEntity)
+				.where(
+					qLoanEntity.memberEntity.eq(memberEntity)
+						.and(qLoanEntity.bookEntity.eq(bookEntity))
+				).fetchOne()
+		).orElseThrow(() -> new NotFoundRequestException(ErrorResult.LOAN_BOOK_NOT_FOUND_EXCEPTION));
+	}
+
+	@Override
+	public void delete(LoanEntity loanEntity) {
+		loanEntityJpaRepository.delete(loanEntity);
+	}
 }
