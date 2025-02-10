d7171a512d7fea7b7015e73b2f30fefafa07b640
euics <pjh575157@gmail.com>
2025-02-10 09:52:11 +0900

    feat: LoanEntity 엔티티 추가 (#1)
    - LoanEntity 엔티티 클래스 생성
      - bookSeq, memberEntity, bookEntity, loanDate, dueDate, returnDate 필드 추가
      - ManyToOne 관계를 통해 MemberEntity 및 BookEntity와 연결
      - @DynamicInsert 및 @DynamicUpdate 적용

---
+++ infrastructure/src/main/java/assignment/application/infrastructure/loan/entity/LoanEntity.java
@@ -0,0 +1,54 @@
+package assignment.application.infrastructure.loan.entity;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import assignment.application.infrastructure.book.entity.BookEntity;
+import assignment.application.infrastructure.member.entity.MemberEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "LOAN")
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoanEntity {
+	@Id
+	@Column(name = "loan_seq")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long bookSeq;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_seq")
+	private MemberEntity memberEntity;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "book_seq")
+	private BookEntity bookEntity;
+
+	@Column(nullable = false, name = "loan_date")
+	private LocalDate loanDate;
+
+	@Column(nullable = false, name = "due_date")
+	private LocalDate dueDate;
+
+	@Column(nullable = false, name = "return_date")
+	private LocalDate returnDate;
+}
