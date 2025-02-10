2a2470e3b25edb5c6f776e166885fd780042a2de
euics <pjh575157@gmail.com>
2025-02-10 12:11:37 +0900

    feat: 회원 저장 시 중복 ID 검사 추가 (#3)
    - MemberEntityRepository 인터페이스 및 구현체(MemberEntityRepositoryImpl) 추가
    - save 메서드를 통해 회원 정보 저장 기능 구현
    - isDuplicatedId 메서드를 통해 중복된 ID 검사 및 ConflictException 발생
    - MemberEntityJpaRepository를 활용하여 DB에서 ID 중복 여부 확인

--- infrastructure/src/main/java/assignment/application/infrastructure/member/repository/MemberEntityRepositoryImpl.java
+++ infrastructure/src/main/java/assignment/application/infrastructure/member/repository/MemberEntityRepositoryImpl.java
@@ -2,6 +2,26 @@ package assignment.application.infrastructure.member.repository;
 
 import org.springframework.stereotype.Component;
 
+import assignment.application.exception.message.ErrorResult;
+import assignment.application.exception.status.ConflictException;
+import assignment.application.infrastructure.member.entity.MemberEntity;
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class MemberEntityRepositoryImpl implements MemberEntityRepository {
+	private final MemberEntityJpaRepository memberEntityJpaRepository;
+
+	@Override
+	public MemberEntity save(MemberEntity memberEntity) {
+		return memberEntityJpaRepository.save(memberEntity);
+	}
+
+	@Override
+	public void isDuplicatedId(String id) {
+		memberEntityJpaRepository.findById(id)
+			.ifPresent(entity -> {
+				throw new ConflictException(ErrorResult.ID_DUPLICATION_CONFLICT_EXCEPTION);
+			});
+	}
 }
